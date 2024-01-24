5c72dd750bcb5451e5869f92e10f6b5ccb6e73a0
lildhansen <madslh@yahoo.com>
2024-01-24 22:24:40 +0100

    fixed parenthesis and split into functions and added comments. Also now it can be done with as many numbers as you want

--- randomNumberStuff.py
+++ randomNumberStuff.py
@@ -1,9 +1,6 @@
 from random import randint
 from random import shuffle
 
-#still some errors, but works pretty well
-#parenteser når vi har subexpressions er fucked
-
 class Result:
     def __init__(self, nums, acc,exp):
         self.usedNums = nums
@@ -17,72 +14,80 @@ class Result:
         return self.acc == other.acc
 
 
-def main(a,b,c,d):
+def getResults(originalNumbers):
     results = []
     subExpressions = []
-    for i in range(1000):
-        nums = [a,b,c,d]
+    for _ in range(2000):
+        nums = originalNumbers.copy()
         usedNums = []
         acc = 0
         expression = ""
         while len(nums) > 0:
-            res = False
-            #the number
-            if subExpressions == []:
-                i = randint(0, len(nums)-1)
-            else:
-                i = randint(-1, len(nums)-1)
-                 
-            if i == -1:
-                shuffle(subExpressions)
-                for subExpression in subExpressions:
-                    if not any(x in subExpression.usedNums for x in usedNums):
-                        res = performOperation(expression,acc,subExpression.acc)
-                        if res != False: #if it is possible to perform the operation
-                            acc = res
-                            expression = "(" + expression + subExpression.expression + ")"
-                            usedNums.extend(subExpression.usedNums)
-                            nums = [x for x in nums if x not in subExpression.usedNums]
-                            break
-                #if it was not possible to perform any operations with a subexpression
-                expression = expression[:-1]
-            else:
-                num = nums[i]
-                res = performOperation(expression,acc,num)
-                if res == False: #if it is not possible to perform the operation
-                    expression = expression[:-1]
-                else:
-                    acc = res    
-                    num = nums.pop(i)
-                    usedNums.append(num)
-                    expression = "(" + expression + str(num) + ")"
+            #handling the the number - not sure why i have to return all values and the function doesnt change them
+            (expression,subExpressions,nums,usedNums,acc) = addNumber(expression,subExpressions,nums,usedNums,acc)
             if len(nums) == 0:
                 break
-            #the operator
-            i = randint(-1,3)
-            if i == -1:
-                break #stop
-            elif i == 0:
-                expression += "+"
-            elif i == 1:
-                expression += "-"
-            elif i == 2:
-                expression += "*"
-            elif i == 3:
-                expression += "/"
-        addSubExpression = True
-        for subExpression in subExpressions:
-            if expression == subExpression.expression:
-                addSubExpression = False
+            #handling the operator
+            result = addOperator(expression)
+            if result == False: #if no operator should be added, and that expression is finished
                 break
-        if addSubExpression:
+            else:
+                expression = result
+                
+        if not any(subExpression.expression==expression for subExpression in subExpressions):
             subExpressions.append(Result(usedNums, acc, expression))
         if not any(result.acc==acc for result in results):
             results.append(Result(usedNums, int(acc), expression))
-    results.sort()
     return results
+    
+def addOperator(expression):
+    i = randint(-1,3)
+    if i == -1:
+        return False #stop
+    elif i == 0:
+        expression += "+"
+    elif i == 1:
+        expression += "-"
+    elif i == 2:
+        expression += "*"
+    elif i == 3:
+        expression += "/"
+    return expression
 
-        
+def addNumber(expression,subExpressions,nums,usedNums,acc):
+    if subExpressions == []:
+        i = randint(0, len(nums)-1)
+    else:
+        i = randint(-1, len(nums)-1)
+            
+    if i == -1: #add subexpression rather than a number
+        shuffle(subExpressions)
+        performedOperation = False
+        for subExpression in subExpressions:
+            if not any(x in subExpression.usedNums for x in usedNums):
+                res = performOperation(expression,acc,subExpression.acc)
+                if res != False: #if it is possible to perform the operation
+                    acc = res
+                    expression = "(" + expression + subExpression.expression + ")"
+                    usedNums.extend(subExpression.usedNums)
+                    nums = [x for x in nums if x not in subExpression.usedNums]
+                    performedOperation = True
+                    break
+        #if it was not possible to perform any operations with a subexpression
+        if not performedOperation:
+            expression = expression[:-1]
+    else: #add number corresponding to index in list
+        num = nums[i]
+        res = performOperation(expression,acc,num)
+        if res == False: #if it is not possible to perform the operation
+            expression = expression[:-1]
+        else:
+            acc = res    
+            num = nums.pop(i)
+            usedNums.append(num)
+            expression = "(" + expression + str(num) + ")"   
+    return (expression,subExpressions,nums,usedNums,acc)
+    
 def performOperation(expression,acc,num):
     if expression == "" or expression[-1] == "+":
         acc += num
@@ -99,8 +104,6 @@ def performOperation(expression,acc,num):
     return acc
       
       
-                    
-
 def printResults(results,verbose=False):
     if results == []:
         print("No results")
@@ -111,14 +114,10 @@ def printResults(results,verbose=False):
             if verbose:
                 print(results[results.index(i)].expression,end=" = ")
             print(i)
-            
         else:
             return
         i += 1
-        
 
-        
-        # print(result.acc)
-        # print(result.expression)
-results = main(3,6,10,12)
+results = getResults([1,2,3,4])
 printResults(results,verbose=True)
+
